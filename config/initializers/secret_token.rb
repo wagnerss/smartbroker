4188f01f5fbbcdef5abdd50507feb72e9dba3c02
Wagner Soares de Souza <wagnerss@gmail.com>
2011-11-08 18:53:03 -0200

    primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Smartbroker::Application.config.secret_token = '042f7fce784898c8e7bc16de627964422479966787c155f866cb5d62fd0367bf894110de0488b56a2ef4d3fc4af52c93925b57ad7fbbe3a21ae5e4292a7dc322'
